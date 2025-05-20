ce663dbf30566f278ec04912f1657367cf66d63f
Alexander Boldt <boldt.alexander@outlook.com>
2025-05-20 16:29:10 +0200

    moved rabbitmq-queue name to configuration-file

--- src/main/kotlin/com/alex/munchies/configuration/RabbitMqConfiguration.kt
+++ src/main/kotlin/com/alex/munchies/configuration/RabbitMqConfiguration.kt
@@ -1,16 +1,15 @@
 package com.alex.munchies.configuration
 
 import org.springframework.amqp.core.Queue
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class RabbitMqConfiguration {
-
-    companion object {
-        const val queue = "statistic"
-    }
+class RabbitMqConfiguration(@Value("\${values.rabbitmq.queue}") private val queue: String) {
 
     @Bean
-    fun queue(): Queue = Queue(queue, false)
+    fun queue(): Queue {
+        return Queue(queue, false)
+    }
 }
--- src/main/kotlin/com/alex/munchies/service/RabbitMqProducer.kt
+++ src/main/kotlin/com/alex/munchies/service/RabbitMqProducer.kt
@@ -1,17 +1,17 @@
 package com.alex.munchies.service
 
-import com.alex.munchies.configuration.RabbitMqConfiguration
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class RabbitMqProducer {
+class RabbitMqProducer(@Value("\${values.rabbitmq.queue}") private val queue: String) {
 
     @Autowired
     private lateinit var rabbitTemplate: RabbitTemplate
 
     fun sendMessage(message: String) {
-        rabbitTemplate.convertAndSend( RabbitMqConfiguration.Companion.queue, message)
+        rabbitTemplate.convertAndSend( queue, message)
     }
 }
