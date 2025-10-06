0ccd834e3f32631f48c832d00113c3078de8caee
Alexander Boldt <boldt.alexander@outlook.com>
2025-10-06 13:41:34 +0200

    #17 created update-function in controller

--- src/main/kotlin/com/alex/munchies/controller/StepController.kt
+++ src/main/kotlin/com/alex/munchies/controller/StepController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -31,6 +32,15 @@ class StepController(private val stepService: StepService) {
 
     @GetMapping(Path.ID)
     fun read(@PathVariable("recipe_id") recipeId: Long, @PathVariable id: Long) = stepService.read(id, recipeId)
+
+    // update
+
+    @PutMapping(Path.ID)
+    fun update(
+        @PathVariable("recipe_id") recipeId: Long,
+        @PathVariable id: Long,
+        @RequestBody step: Step
+    ) = stepService.update(id, recipeId, step)
 
     // delete
 
