3d2e0e7f1d880b62b23443b81ae7627daef129cb
Alexander Boldt <boldt.alexander@outlook.com>
2025-10-09 17:45:44 +0200

    combined step-domain-models in a file

--- src/main/kotlin/com/alex/munchies/domain/Step.kt
+++ src/main/kotlin/com/alex/munchies/domain/Step.kt
@@ -1,5 +1,11 @@
 package com.alex.munchies.domain
 
+data class StepRequest(
+    val number: Int,
+    val title: String,
+    val description: String
+)
+
 data class StepResponse(
     val id: Long,
     val userId: String,
--- src/main/kotlin/com/alex/munchies/domain/StepRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.alex.munchies.domain
-
-data class StepRequest(
-    val number: Int,
-    val title: String,
-    val description: String
-)
