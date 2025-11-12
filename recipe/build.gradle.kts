5e2ffc37bba39496addce9851ec4592d9a50cb40
Alexander Boldt <boldt.alexander@outlook.com>
2025-11-12 15:41:22 +0100

    close #20; created multi module project with recipe-microservice and gateway

---
+++ recipe/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.spring)
+	alias(libs.plugins.kotlin.jpa)
+	alias(libs.plugins.spring.boot)
+	alias(libs.plugins.spring.dependency.management)
+}
+
+group = "com.alex"
+
+kotlin {
+	jvmToolchain(21)
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// development libraries
+	implementation(libs.kotlin.reflect)
+	implementation(libs.kotlin.stdlib)
+	implementation(libs.jackson.kotlin)
+
+	implementation(libs.spring.boot.starter.web)
+	implementation(libs.spring.boot.starter.data)
+	implementation(libs.spring.boot.starter.oauth2.resource)
+	implementation(libs.spring.boot.starter.security)
+	implementation(libs.spring.boot.starter.aop)
+
+	implementation(libs.mysql)
+	implementation(libs.flyway.core)
+	implementation(libs.flyway.mysql)
+
+	implementation(libs.openfeign)
+
+    implementation(libs.aws.s3)
+
+    implementation(libs.resilience4j)
+
+	implementation(libs.springdoc)
+
+	developmentOnly(libs.spring.boot.devtools)
+
+	// test libraries
+	testImplementation(libs.spring.boot.starter.test) {
+		exclude(module = "junit")
+	}
+
+    testImplementation(libs.mockito.kotlin)
+
+	testImplementation(libs.kotest.runner.junit)
+	testImplementation(libs.kotest.assertions.core)
+
+	testImplementation(libs.testcontainers.junit.jupiter)
+
+	testImplementation(libs.testcontainers.mysql)
+    testImplementation(libs.testcontainers.minio)
+	testImplementation(libs.rest.assured)
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
