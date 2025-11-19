db9040b3f84237afc09dfc7476b25eedbd520367
Alexander Boldt <boldt.alexander@outlook.com>
2025-11-19 11:03:27 +0100

    close #24; created empty library module

---
+++ common/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.alex"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "munchies"
 
 include("gateway")
 include("recipe")
+include("common")
