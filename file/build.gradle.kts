56aba14873c4672277ffdf5e53f702c01c00b9d6
Alexander Boldt <boldt.alexander@outlook.com>
2025-12-18 10:54:42 +0100

    added dependency for spring-context; added s3-functionality

--- file/build.gradle.kts
+++ file/build.gradle.kts
@@ -8,6 +8,13 @@ dependencies {
     // development libraries
     implementation(libs.kotlin.reflect)
     implementation(libs.kotlin.stdlib)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.reactor)
+
+    implementation(libs.spring.context)
+
+    implementation(libs.aws.s3)
 }
 
 tasks.test {
--- recipe/src/main/kotlin/org/munchies/service/S3Bucket.kt
+++
@@ -1,8 +0,0 @@
-package org.munchies.service
-
-/**
- * Represents the buckets in the S3-storage.
- */
-enum class S3Bucket(val bucketName: String) {
-    RECIPE("recipe")
-}
